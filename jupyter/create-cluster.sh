d3cb63722c3ca3b2b93344c782eae94192665d1e
Behzad Samadi <bsamadi@connected.io>
2018-11-07 14:03:20 -0500

    Remove Jupyter port

--- jupyter/create-cluster.sh
+++ jupyter/create-cluster.sh
@@ -3,7 +3,6 @@
 export PROJECT=ontario-2018
 export BUCKET=ontario-2018-dataproc
 export DATAPROC_CLUSTER_NAME=mydataproc
-export JUPYTER_PORT=8080
 
 gcloud beta dataproc clusters create ${DATAPROC_CLUSTER_NAME} \
   --project ${PROJECT} \
