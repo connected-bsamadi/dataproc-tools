603bb6c10f6e4ad761bec110ce749e1325ae70a3
Behzad Samadi <bsamadi@connected.io>
2018-11-07 13:31:45 -0500

    Adding dependency jars

--- jupyter/create-cluster.sh
+++ jupyter/create-cluster.sh
@@ -9,4 +9,11 @@ gcloud beta dataproc clusters create ${DATAPROC_CLUSTER_NAME} \
   --project ${PROJECT} \
   --bucket ${BUCKET} \
   --optional-components=ANACONDA,JUPYTER \
-  --image-version=1.3-deb9
+  --image-version=1.3-deb9 \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-base_2.11:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-core_2.11:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-runtime_2.11:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark_2.11:0.12.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark-base_2.11:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-tensor_2.11:0.10.0' \
+  --properties spark:spark.jars.packages='com.databricks:spark-avro_2.11:4.0.0' 
