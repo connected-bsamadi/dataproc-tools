644dfecbfb0a5922aee01544c267a11345abdf71
Behzad Samadi <bsamadi@connected.io>
2018-11-07 16:16:13 -0500

    Making MLeap versions the same

--- jupyter/create-cluster.sh
+++ jupyter/create-cluster.sh
@@ -12,7 +12,7 @@ gcloud beta dataproc clusters create ${DATAPROC_CLUSTER_NAME} \
   --properties spark:spark.jars.packages='ml.combust.mleap:mleap-base_2.11:0.10.0' \
   --properties spark:spark.jars.packages='ml.combust.mleap:mleap-core_2.11:0.10.0' \
   --properties spark:spark.jars.packages='ml.combust.mleap:mleap-runtime_2.11:0.10.0' \
-  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark_2.11:0.12.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark_2.11:0.10.0' \
   --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark-base_2.11:0.10.0' \
   --properties spark:spark.jars.packages='ml.combust.mleap:mleap-tensor_2.11:0.10.0' \
   --properties spark:spark.jars.packages='com.databricks:spark-avro_2.11:4.0.0' 
