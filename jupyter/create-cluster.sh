94dfd3530c08806087503fdf5a12234ac9956f66
Behzad Samadi <bsamadi@connected.io>
2018-11-08 11:05:43 -0500

    Changing the version to 12

--- jupyter/create-cluster.sh
+++ jupyter/create-cluster.sh
@@ -9,10 +9,10 @@ gcloud beta dataproc clusters create ${DATAPROC_CLUSTER_NAME} \
   --bucket ${BUCKET} \
   --optional-components=ANACONDA,JUPYTER \
   --image-version=1.3 \
-  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-base_2.11:0.10.0' \
-  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-core_2.11:0.10.0' \
-  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-runtime_2.11:0.10.0' \
-  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark_2.11:0.10.0' \
-  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark-base_2.11:0.10.0' \
-  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-tensor_2.11:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-base_2.12:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-core_2.12:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-runtime_2.12:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark_2.12:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-spark-base_2.12:0.10.0' \
+  --properties spark:spark.jars.packages='ml.combust.mleap:mleap-tensor_2.12:0.10.0' \
   --properties spark:spark.jars.packages='com.databricks:spark-avro_2.11:4.0.0' 
